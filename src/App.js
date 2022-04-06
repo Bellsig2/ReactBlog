bca3acd0cca7d3344b8e3dbb27e00778d735d738
Bellsig2 <zhzhdkspt@gmail.com>
2022-04-07 00:01:20 +0900

    prettier 자동정렬 수정
    건방지게 자꾸 줄을 바꾸길래 수정

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  let [title, changeTitle] = useState([
-    "남자 코트 추천",
-    "여자 코트 추천",
-    "아동 코트 추천",
-  ]);
+  let [title, changeTitle] = useState(["남자 코트 추천", "여자 코트 추천", "아동 코트 추천"]);
+  let [good, countGood] = useState(0);
+  function change() {
+    changeTitle(["여자 코트 추천", "여자 코트 추천", "아동 코트 추천"]);
+  }
   return (
     <div className="App">
       <div style={{ backgroundColor: "black" }} className="blackNav">
@@ -17,12 +17,16 @@ function App() {
       </div>
       <div className="writeWrap">
         <div className="goDetail">
-          <h3>{title[0]}</h3>
+          <h3>
+            {title[0]}
+            <span onClick={() => countGood(good++)}>💚{good}</span>
+          </h3>
           <p>2월 17일 발행</p>
         </div>
         <div className="goDetail">
           <h3>{title[1]}</h3>
           <p>2월 17일 발행</p>
+          <button onClick={() => change()}>버튼</button>
         </div>
         <div className="goDetail">
           <h3>{title[2]}</h3>
